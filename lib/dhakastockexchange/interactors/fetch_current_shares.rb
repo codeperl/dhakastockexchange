85ae67a3b2a8fd2ea8e02b8863e53480eea8ca2b
Mohammad Ashrafuddin Ferdousi <it.codeperl@gmail.com>
2017-12-21 22:51:12 +0600

    Interactor implemented for hanami!

--- lib/dhakastockexchange/interactors/fetch_current_shares.rb
+++ lib/dhakastockexchange/interactors/fetch_current_shares.rb
@@ -9,22 +9,23 @@ class FetchCurrentShares
 
   def initialize;end
 
+  # rubocop:disable Metrics/AbcSize
   def call
     @shares = parse_url('http://www.dsebd.org/latest_share_price_all.php')
                               .css('body div table')
                               .children
                               .map do |child|
-      {}.tap do |row|
-        row[:trading_code] = child.css('td')[1].text.strip
-        row[:last_traded_price_for_today] = child.css('td')[2].text.strip.to_f
-        row[:highest_price_for_today] = child.css('td')[3].text.strip.to_f
-        row[:lowest_price_for_today] = child.css('td')[4].text.strip.to_f
-        row[:closing_price_for_today] = child.css('td')[5].text.strip.to_f
-        row[:yesterdays_closing_price] = child.css('td')[6].text.strip.to_f
-        row[:change_for_today] = child.css('td')[7].text.strip.to_f
-        row[:trade_for_today] = child.css('td')[8].text.strip.to_f
-        row[:value_million_for_today] = child.css('td')[9].text.strip.to_f
-        row[:volume_for_today] = child.css('td')[10].text.strip
+      {}.tap do |share|
+        share[:trading_code]                = child.css('td')[1].text.strip
+        share[:last_traded_price_for_today] = child.css('td')[2].text.strip.to_f
+        share[:highest_price_for_today]     = child.css('td')[3].text.strip.to_f
+        share[:lowest_price_for_today]      = child.css('td')[4].text.strip.to_f
+        share[:closing_price_for_today]     = child.css('td')[5].text.strip.to_f
+        share[:yesterdays_closing_price]    = child.css('td')[6].text.strip.to_f
+        share[:change_for_today]            = child.css('td')[7].text.strip.to_f
+        share[:trade_for_today]             = child.css('td')[8].text.strip.to_f
+        share[:value_million_for_today]     = child.css('td')[9].text.strip.to_f
+        share[:volume_for_today]            = child.css('td')[10].text.strip
       end
     end
 
