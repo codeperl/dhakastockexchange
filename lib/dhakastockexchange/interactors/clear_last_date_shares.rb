206afcfa3897cfa2f9d3c1692e570a8ae91a7a93
Mohammad Ashrafuddin Ferdousi <it.codeperl@gmail.com>
2017-12-26 14:34:22 +0600

    Adding 2 new tables for todays shares and current time shares.

--- lib/dhakastockexchange/interactors/clear_last_date_shares.rb
+++ lib/dhakastockexchange/interactors/clear_last_date_shares.rb
@@ -1,13 +1,17 @@
 require 'hanami/interactor'
+require_relative './../constants/office'
 
 class ClearLastDateShares
   include Hanami::Interactor
+  include Office
 
   def initialize(current_date_share_repository: CurrentDateShareRepository.new)
     @current_date_share_repository = current_date_share_repository
   end
 
   def call
-    @current_date_share_repository.clear
+    if Office::OPEN == @office_hour_interactor.call.office_hour
+      @current_date_share_repository.clear
+    end
   end
 end
