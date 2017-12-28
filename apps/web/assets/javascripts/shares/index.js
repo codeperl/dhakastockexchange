38296bffca9c0dcaf2b919ec07d10a8aa2b29914
Mohammad Ashrafuddin Ferdousi <it.codeperl@gmail.com>
2017-12-29 00:51:50 +0600

    Fixing colors of rows in share updates.

--- apps/web/assets/javascripts/shares/index.js
+++ apps/web/assets/javascripts/shares/index.js
@@ -1,7 +1,24 @@
 jQuery(document).ready(function () {
-    var faye = new Faye.Client('http://localhost:8080/faye'); // FIXME! ROMAN!
-
-    faye.subscribe('/messages/all', function (content) {
-        $('#share-updates').html(content);
-    });
+    notify(setClient('http://localhost:8080/faye'), '/messages/all', '#share-updates'); // FIXME! ROMAN! Faye server!
 });
+
+/**
+ * @param url
+ * @returns {*}
+ */
+function setClient(url)
+{
+    return new Faye.Client(url);
+}
+
+/**
+ * @param client
+ * @param channel
+ * @param container
+ */
+function notify(client, channel, container)
+{
+    client.subscribe(channel, function (content) {
+        $(container).html(content);
+    });
+}
