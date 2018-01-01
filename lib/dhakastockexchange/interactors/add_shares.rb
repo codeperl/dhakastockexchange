04c6f65e4d40f077a31ee87b7e5ae5c727e623cf
Mohammad Ashrafuddin Ferdousi <it.codeperl@gmail.com>
2018-01-01 19:19:24 +0600

    Add extra information about shares in share related database tables.

--- lib/dhakastockexchange/interactors/add_shares.rb
+++ lib/dhakastockexchange/interactors/add_shares.rb
@@ -1,3 +1,4 @@
+require 'time'
 require 'hanami/interactor'
 require_relative './../constants/office'
 
@@ -30,45 +31,61 @@ class AddShares
 
   def call
     if Office::OPEN == @office_hour_interactor.call.office_hour
-      fetched_shares = @fetch_current_shares_interactor.call.shares
-
-      unless fetched_shares.empty?
-        share_update_version = Hash.new
-        share_update_version[:fake_data] = nil
-        last_share_update_version = @share_update_version_repository.create(share_update_version)
-
-        @clear_last_time_shares_interactor.call
-        fetched_shares.each do |share|
-
-          searched_share = @share_repository.find_one_by(
-              share[:trading_code], previous_version(last_share_update_version.version)
-          )
-
-          last_traded_price_change_than_last_update = calculate_last_traded_price_change_than_last_update(
-              searched_share, share[:last_traded_price_for_today]
-          )
-
-          value_in_million_change_than_last_update = calculate_value_in_million_change_than_last_update(
-              searched_share, share[:value_million_for_today]
-          )
-
-          traded_change_than_last_update = calculate_traded_change_than_last_update(
-              searched_share, share[:trade_for_today]
-          )
-
-          share[:version] = last_share_update_version.version
-          share[:last_traded_price_change_than_last_update] = last_traded_price_change_than_last_update
-          share[:value_in_million_change_than_last_update] = value_in_million_change_than_last_update
-          share[:traded_change_than_last_update] = traded_change_than_last_update
-
-          @add_current_time_share_interactor.call(share)
-          @add_current_date_share_interactor.call(share)
-          @add_share_interactor.call(share)
-        end
-      end
+      generate_shares_information
+    else
+      generate_day_closing_share_information
     end
 
     @shares = @current_time_share_repository.all
+  end
+
+  def generate_day_closing_share_information
+    closing_start_time = Time.parse "2:31 pm"
+    closing_end_time = Time.parse "2:35 pm"
+    current_time = Time.now
+
+    if current_time >= closing_start_time && current_time <=closing_end_time
+      generate_shares_information
+    end
+  end
+
+  def generate_shares_information
+    fetched_shares = @fetch_current_shares_interactor.call.shares
+
+    unless fetched_shares.empty?
+      share_update_version = Hash.new
+      share_update_version[:fake_data] = nil
+      last_share_update_version = @share_update_version_repository.create(share_update_version)
+
+      @clear_last_time_shares_interactor.call
+      fetched_shares.each do |share|
+
+        searched_share = @share_repository.find_one_by(
+            share[:trading_code], previous_version(last_share_update_version.version)
+        )
+
+        last_traded_price_change_than_last_update = calculate_last_traded_price_change_than_last_update(
+            searched_share, share[:last_traded_price_for_today]
+        )
+
+        value_in_million_change_than_last_update = calculate_value_in_million_change_than_last_update(
+            searched_share, share[:value_million_for_today]
+        )
+
+        traded_change_than_last_update = calculate_traded_change_than_last_update(
+            searched_share, share[:trade_for_today]
+        )
+
+        share[:version] = last_share_update_version.version
+        share[:last_traded_price_change_than_last_update] = last_traded_price_change_than_last_update
+        share[:value_in_million_change_than_last_update] = value_in_million_change_than_last_update
+        share[:traded_change_than_last_update] = traded_change_than_last_update
+
+        @add_current_time_share_interactor.call(share)
+        @add_current_date_share_interactor.call(share)
+        @add_share_interactor.call(share)
+      end
+    end
   end
 
   def previous_version(current_version)
